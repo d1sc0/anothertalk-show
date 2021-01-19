a713880f2a472892655bc0c5c84d853398a9871b
Stuart Mackenzie <ilovediscostu@gmail.com>
2021-01-19 17:28:17 +0000

    updating styles

--- src/pages/about.js
+++ src/pages/about.js
@@ -20,12 +20,13 @@ const AboutPage = ({ data }) => {
             Do you love talk shows but find the content a little bland and formulaic? Are you a little fed up of manicured celebrities promoting their latest ventures, sticking to pre-determined questions under glitzy studio lighting. If so, this podcast might be for you. 
           </p>
           <p>
-            <strong>The aim of the show is to simply make connections with and tease out interesting and entertaining stories from real people who have a particular talent, hobby or are doing interesting things in the world. People who are trying to make the world a little bit brighter or better.</strong> You won't find interviews here with people who have fame, fortune or immense power. They already get plenty of air-time elsewhere.
+            <strong>The aim of the show is to simply make connections with and tease out interesting and entertaining stories from real people who have a particular talent, hobby or are doing interesting things in the world. People who are trying to make the world a little bit brighter or better.</strong> 
           </p>
+          <p>You won't find interviews here with people who have fame, fortune or immense power. They already get plenty of air-time elsewhere.</p>
           <p>
-            It's not a show expecting to reach a large audience and at this point consider it more of an open experiment - Maybe my Mum will listen? I'm hopeful that by sharing the show on the internet that over time it'll build up a tiny but like-minded audience that can help me make new connections and unlock access to other interesting guests and entertaining content. <strong>The success of the experiment will be very much reliant on the good will of potential guests but I reckon the world has more of this good will than we may think.</strong>
+            It's not a show expecting to reach a large audience and at this point consider it more of an open experiment - Maybe my Mum will listen? I'm hopeful that by sharing the show on the internet that over time it'll build up a tiny but like-minded audience that can help me make new connections and unlock access to other interesting guests and entertaining content. 
           </p>
-          
+          <p><strong>The success of the experiment will be very much reliant on the good will of potential guests but I reckon the world has more of this good will than we may think.</strong></p>
          <h3 id="host">Meet the host</h3>
             <p>
             <Image
@@ -37,8 +38,9 @@ const AboutPage = ({ data }) => {
             </p>
             <p>You can find more about me on my <a href="https://hellostu.xyz">personal site and blog.</a></p>
             <p>
-             The podcast is a hobby project partly inspired by my day job working in consulting roles. The variety of things I've worked on over the last 4 years has allowed me to be introduced to many interesting, creative, hard-working, passionate, talented people who all have stories to share from both their work or personal lives. <strong>I'm hoping to use this as a space to explore these stories in an light and entertaining way, learn from them and take inspiration. That's a lofty goal - I'll also settle for a pleasant natter over a nice cup of tea!</strong>
+             The podcast is a hobby project partly inspired by my day job working in consulting roles. The variety of things I've worked on over the last 4 years has allowed me to be introduced to many interesting, creative, hard-working, passionate, talented people who all have stories to share from both their work or personal lives. 
           </p>
+          <p><strong>I'm hoping to use this as a space to explore these stories in an light and entertaining way, learn from them and take inspiration. That's a lofty goal - I'll also settle for a pleasant natter over a nice cup of tea!</strong></p>
             <h3 id="get-involved">Ways you can get involved</h3>
             <p>
               <strong>1. Listen, Subscribe, Share, Give feedback</strong> - Knowing people are listening to the show and getting something out of this little experiment will help motivate me to put more time and energy into it. You'll find relevant links at the bottom of each website page.</p>
